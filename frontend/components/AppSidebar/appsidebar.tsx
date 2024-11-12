51dee9a146e2fba9d3a3bf27d0beb94444f79752
Lanz Angeles <masterofbodyshots@gmail.com>
2024-11-12 00:22:10 -0500

    whoops

--- frontend/components/AppSidebar/appsidebar.tsx
+++ frontend/components/AppSidebar/appsidebar.tsx
@@ -1,24 +1,18 @@
-"use client";
+"use client"
 
 import { AuthSidebar } from "./AuthSidebar/authsidebar";
 import { GuestSidebar } from "./GuestSidebar/guestsidebar";
-import { checkAuth } from "@/lib/checkauth";
+import { useAuth } from '../../contexts/AuthContext'
+import { useState, useEffect } from 'react';
 import './appsidebar.css';
-import { useState, useEffect } from "react";
 
-export const AppSidebar = () => {
-  const [isAuth, setIsAuth] = useState<boolean | null>(null);
+export function AppSidebar() {
+  const [sideBar, setSideBar] = useState(<GuestSidebar/>);
+  const { isAuth } = useAuth();
 
   useEffect(() => {
-    const fetchAuthStatus = async () => {
-      const authStatus = await checkAuth();
-      setIsAuth(authStatus);
-    };
+    setSideBar((isAuth) ? (<AuthSidebar/>) : (<GuestSidebar/>))
+  }, [isAuth])
 
-    fetchAuthStatus();
-  }, []);
-
-  if (isAuth === null) return null; // TODO: Loading spinner?
-
-  return isAuth ? <AuthSidebar /> : <GuestSidebar />;
+  return ( sideBar )
 }
